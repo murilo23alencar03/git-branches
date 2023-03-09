b6f2b7b367bbf8e34156d1a30f98f16522299b6a
Murilo <murilo.jose@germinare.org.br>
2023-03-09 20:55:59 -0300

    Corrigindo a merge novamente

--- Cliente.java
+++ Cliente.java
@@ -1,10 +1,8 @@
 public class Cliente {
-<<<<<<< HEAD
     private String tipoConta;
-=======
     private String nome;
     private String cpf;
     private int agencia;
     private double saldo;
->>>>>>> classe-cliente
+
 }
